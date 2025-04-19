120a8b56d8a0db70f9877311da187ba11ddc5a2b
Dan Ngo <danngo.au@gmail.com>
2025-04-19 21:21:07 +1000

    clean up

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+
+export default [
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{js,mjs,jsx,vue}"],
+  },
+
+  {
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+  },
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  js.configs.recommended,
+
+  ...pluginVue.configs["flat/essential"],
+
+  {
+    rules: {
+      ...pluginVue.configs["flat/essential"].rules,
+      "vue/multi-word-component-names": "off", // Turn off multi-word component rule
+    },
+  },
+];
