00a505f90253cd07d5bbe85798f1d818b2482264
Dan Ngo <danngo.au@gmail.com>
2025-04-21 21:16:07 +1000

    add footer + nav bar enhancement

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,1 +1,12 @@
-<template></template>
+<template>
+  <footer class="bg-primary-subtle text-dark py-3 mt-auto text-center">
+    <p>
+      &copy; 2025 Insight Hire. Developed by
+      <a
+        :href="'https://www.danngo.space'"
+        class="text-decoration-underline"
+        target="_blank"
+      >Dan Ngo</a>.
+    </p>
+  </footer>
+</template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,41 +16,48 @@ const pages = [
 </script>
 
 <template>
-  <header class="d-flex flex-row align-items-center justify-content-between px-5 py-2 bg-primary-subtle">
-    <a class="fs-4 fw-bold bg-transparent cursor-pointer" :href=BASE_URL>
-      Insight Hire
-    </a>
-
-    <nav class="d-none d-lg-flex gap-3 align-items-center">
-      <RouterLink to="/">           Job Explorer   </RouterLink>
-      <RouterLink to="/application">Job Application</RouterLink>
-      <RouterLink to="/todo">       To Do List     </RouterLink>
-    </nav>
-
-    <section class="d-flex d-lg-none align-items-center">
-      <button class="btn btn-link p-0" @click="menuRef.toggle($event)">
-        <i class="fas fa-bars fs-3"></i>
-      </button>
-
-      <TieredMenu
-        :model="pages"
-        popup
-        ref="menuRef"
-        class="custom-tiered-menu"
+  <header class="d-flex flex-column">
+    <section class="d-flex flex-row align-items-center justify-content-between px-5 py-2 bg-primary-subtle">
+      <a
+        class="fs-4 fw-bold bg-transparent cursor-pointer"
+        :href="BASE_URL"
       >
-        <template #item="{ item }">
-          <RouterLink
-            :to="item.route"
-            custom
-          >
-            <a :href="`${BASE_URL}#${item.route}`">
-              <span :class="item.icon"></span>
+        Insight Hire
+      </a>
 
-              <p>{{ item.label }}</p>
-            </a>
-          </RouterLink>
-        </template>
-      </TieredMenu>
+      <nav class="d-none d-lg-flex gap-3 align-items-center">
+        <RouterLink to="/">           Job Explorer   </RouterLink>
+        <RouterLink to="/application">Job Application</RouterLink>
+        <RouterLink to="/todo">       To Do List     </RouterLink>
+      </nav>
+
+      <section class="d-flex d-lg-none align-items-center">
+        <button class="btn btn-link p-0" @click="menuRef.toggle($event)">
+          <i class="fas fa-bars fs-3"></i>
+        </button>
+
+        <TieredMenu
+          :model="pages"
+          popup
+          ref="menuRef"
+          class="custom-tiered-menu"
+        >
+          <template #item="{ item }">
+            <RouterLink :to="item.route" custom>
+              <a :href="`${BASE_URL}#${item.route}`">
+                <span :class="item.icon"></span>
+                <p>{{ item.label }}</p>
+              </a>
+            </RouterLink>
+          </template>
+        </TieredMenu>
+      </section>
     </section>
+
+    <img src="../assets/sky.jpg"
+         alt="Site banner"
+         class="w-100"
+         height="150"
+         style="object-fit: cover;" />
   </header>
 </template>
